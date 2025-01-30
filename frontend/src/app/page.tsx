48de936d88984cdfbe84af72bd906a687911d2f8
sofiarovaris <sofiarovaris@gmail.com>
2025-01-30 18:40:39 -0300

    fix: antd react 19 compatibility

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client"; 
+
+import '@ant-design/v5-patch-for-react-19';
 import TravelRequestsView from './view/TravelRequestsView';
 
-function Home(){ 
-  return (
-    <TravelRequestsView />
-  );
+function Home() { 
+  return <TravelRequestsView />;
 }
 
 export default Home;
