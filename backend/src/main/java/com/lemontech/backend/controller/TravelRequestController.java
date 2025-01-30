83a47b0b4e18170772c55216225a6d9f8b7c6b86
sofiarovaris <sofiarovaris@gmail.com>
2025-01-29 23:33:29 -0300

    feat: add cors as env

--- backend/src/main/java/com/lemontech/backend/controller/TravelRequestController.java
+++ backend/src/main/java/com/lemontech/backend/controller/TravelRequestController.java
@@ -1,6 +1,7 @@
 package com.lemontech.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/travel-requests")
+@CrossOrigin(origins = "${cors.allowed-origins}") 
 public class TravelRequestController {
 
     private static final Logger logger = LoggerFactory.getLogger(TravelRequestController.class);
